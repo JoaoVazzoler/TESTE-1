d3bae563dc2b0b6ed1643dc7710c709ec1d2eb74
JoaoVazzoler <joaovazzoler18@gmail.com>
2024-05-06 17:57:10 -0300

    Create média bimestra EXERCICIO.py
    nota do bimestre exercicio

---
+++ média bimestra EXERCICIO.py
@@ -0,0 +1,8 @@
+nota1 = float( input( "nota bismestral 1"))
+nota2 = float( input( "nota bismestral 2"))
+nota3 = float( input( "nota bismestral 3"))
+nota4 = float( input( "nota bismestral 4"))
+
+média = nota1 + nota2 + nota3 + nota4
+
+print(" a média bimestral é :", média)
