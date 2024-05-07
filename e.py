8c60b1e6772dc3816e0eabc1e4368b1ea08d1a76
JoaoVazzoler <joaovazzoler18@gmail.com>
2024-05-07 18:22:56 -0300

    Create e.py

---
+++ e.py
@@ -0,0 +1,6 @@
+lado = float(input("Insira o valor da lateral do quadrado:"))
+area = lado * lado
+dobro = (lado * lado) * 2
+
+print ("A area é igual a:",area)
+print ("O dobro da area é igual a:",dobro)
